862ef12af3c4fc0825bdb9d512badb9a138dd12e
Jacinda Chen <jrchen@dons.usfca.edu>
2019-11-04 18:13:43 +0000

    Add the scripts to unzip and process fastqc

---
+++ scripts/01_unzip_and_fastqc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# A script to unzip and run fastqc on a folder of fastq.gz
+# Illumina sequence files
+
+# Jacinda Chen
+# October 30, 2019
+# jrchen@dons.usfca.edu
+
+# move the files to the unzipped directory
+echo Copying files...
+cp -vr /data/my-illumina-sequences/raw/*.gz /data/my-illumina-sequences/unzipped
+
+# unzip the files
+echo Unzipping files...
+gunzip /data/my-illumina-sequences/unzipped/*
+
+# run fastqc and put output into output folder
+echo Running fastqc
+fastqc /data/my-illumina-sequences/unzipped/*.fastq --outdir=output/fastqc-results
