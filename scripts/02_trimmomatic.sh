3bde1f21e1e352ef239b101f1e210015447d952d
Jacinda Chen <jrchen@dons.usfca.edu>
2019-11-11 17:38:56 +0000

    Modified for loop for trimming script

--- scripts/02_trimmomatic.sh
+++ scripts/02_trimmomatic.sh
@@ -1,4 +1,11 @@
 # run trimmomatic to throw out bad sequences, trim when quality gets low, or if
 # sequences are too short you will need to turn this into a for loop to process
 # all of your files
-TrimmomaticSE -threads 4 -phred33 /data/YOUR_DIR/YOURFILENAME.fastq /data/trimmed/$(basename -s .fastq YOURFILENAME.fastq).trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:140
+# Create for loops for trimming the data one at a time
+
+for file in /data/my-illumina-sequences/unzipped/JC*.fastq
+do
+	echo Now trimming $file
+	TrimmomaticSE -threads 4 -phred33 $file /data/my-illumina-sequences/trimmed/$(basename -s .fastq $file).trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:140
+	echo Done
+done
