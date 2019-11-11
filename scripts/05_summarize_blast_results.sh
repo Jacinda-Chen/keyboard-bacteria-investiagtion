b662c5d822b671a84cda0b564c6bb059380f0cce
Jacinda Chen <jrchen@dons.usfca.edu>
2019-11-11 17:41:03 +0000

    Modified for loop to summarize each sample

--- scripts/05_summarize_blast_results.sh
+++ scripts/05_summarize_blast_results.sh
@@ -1,2 +1,9 @@
 # use cut, sort, and uniq -c to help you summarize the results from the
 # BLAST search.
+
+for blast in /data/my-illumina-sequences/blast_output/JC*.csv
+do
+	echo Now sorting $blast
+	cut -d, -f1 $blast | sort | uniq -c | sort -n
+	echo Done
+done
