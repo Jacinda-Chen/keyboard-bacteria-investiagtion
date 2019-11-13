9e5536a1967cdc24af4d908a11e148d7dbcc1eaa
Jacinda Chen <jrchen@dons.usfca.edu>
2019-11-13 05:30:45 +0000

    Add shebang

--- scripts/03_convert_from_fastq_to_fasta.sh
+++ scripts/03_convert_from_fastq_to_fasta.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
 # convert to fasta for BLAST
 # you need to modify this to save the converted fasta file to a file
 # instead of printing to the screen
 # you'll need to turn this into a for loop too
+
+# Jacinda Chen
+# November 11, 2019
+# jrchen@dons.usfca.edu
 
 for fastqfile in /data/my-illumina-sequences/trimmed/JC*.fastq
 do
