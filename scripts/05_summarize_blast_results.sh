afb0c935b4132aba9cbc48d171d29eec00fb7474
Jacinda Chen <jrchen@dons.usfca.edu>
2019-11-13 05:31:35 +0000

    Add shebang

--- scripts/05_summarize_blast_results.sh
+++ scripts/05_summarize_blast_results.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 # use cut, sort, and uniq -c to help you summarize the results from the
 # BLAST search.
+
+# Jacinda Chen
+# November 11, 2019
+# jrchen@dons.usfca.edu
 
 for blast in /data/my-illumina-sequences/blast_output/JC*.csv
 do
