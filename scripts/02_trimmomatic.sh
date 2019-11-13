49ba98966a1f5f5a0521938daa7fa6aef90d0dc7
Jacinda Chen <jrchen@dons.usfca.edu>
2019-11-13 05:30:27 +0000

    Add shebang

--- scripts/02_trimmomatic.sh
+++ scripts/02_trimmomatic.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
 # run trimmomatic to throw out bad sequences, trim when quality gets low, or if
 # sequences are too short you will need to turn this into a for loop to process
 # all of your files
 # Create for loops for trimming the data one at a time
+
+# Jacinda Chen
+# November 11, 2019
+# jrchen@dons.usfca.edu
 
 for file in /data/my-illumina-sequences/unzipped/JC*.fastq
 do
