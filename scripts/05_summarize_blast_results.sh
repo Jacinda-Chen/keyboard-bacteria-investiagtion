3002285378e1be75e09027ce925baeeb2bb23d8b
Jacinda Chen <jrchen@dons.usfca.edu>
2019-11-13 17:48:15 +0000

    Fix double quotes for 05 script

--- scripts/05_summarize_blast_results.sh
+++ scripts/05_summarize_blast_results.sh
@@ -9,7 +9,7 @@
 
 for blast in /data/my-illumina-sequences/blast_output/JC*.csv
 do
-	echo Now sorting $blast
-	cut -d, -f1 $blast | sort | uniq -c | sort -n
+	echo Now sorting "$blast"
+	cut -d, -f1 "$blast" | sort | uniq -c | sort -n
 	echo Done
 done
