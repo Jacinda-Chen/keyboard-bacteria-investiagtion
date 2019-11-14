c51612c202914ef2baae67969750eb3878714f88
Jacinda Chen <jrchen@dons.usfca.edu>
2019-11-14 07:02:26 +0000

    Change summary results to include controls

--- scripts/05_summarize_blast_results.sh
+++ scripts/05_summarize_blast_results.sh
@@ -10,7 +10,7 @@
 for blast in /data/my-illumina-sequences/blast_output/JC*.csv
 do
 	echo Now sorting "$blast"
-	cut -d, -f1 "$blast" | sort | uniq -c | sort -n
+	cut -d, -f1 "$blast" | sort | uniq -c | sort -n | tail -15
 	echo Done
 done
 
@@ -19,6 +19,22 @@ echo Now for the controls...
 for blast2 in /data/my-illumina-sequences/blast_output/control*.csv
 do
 	echo Now sorting "$blast2"
-	cut -d, -f1 "$blast2" | sort | uniq -c | sort -n
+	cut -d, -f1 "$blast2" | sort | uniq -c | sort -n | tail -15
 	echo Done
 done
+
+echo Now for the summary of JC*.csv
+
+cut -d, -f1 /data/my-illumina-sequences/blast_output/JC*.csv | sort | uniq -c | sort -n | tail -15
+
+echo Now for the summary of negative controls...
+
+cut -d, -f1 /data/my-illumina-sequences/blast_output/control-neg*.csv | sort | uniq -c | sort -n | tail -15
+
+echo Now for the summary of positive controls...
+
+cut -d, -f1 /data/my-illumina-sequences/blast_output/control-zy*.csv | sort | uniq -c | sort -n | tail -15
+
+echo Now for the summary of everything...
+
+cut -d, -f1 /data/my-illumina-sequences/blast_output/*.csv | sort | uniq -c | sort -n | tail -15
