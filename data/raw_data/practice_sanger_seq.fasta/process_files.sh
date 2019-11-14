192b40b9a6ca7e16240298f5afaf2fa31a0c6f88
Jacinda Chen <jrchen@dons.usfca.edu>
2019-11-14 06:59:55 +0000

    Quote to prevent word splitting of process_files.sh

--- data/raw_data/practice_sanger_seq.fasta/process_files.sh
+++ data/raw_data/practice_sanger_seq.fasta/process_files.sh
@@ -12,20 +12,20 @@ echo "Starting for loop..."
 # this is a for loop
 for file in *file*.txt
 do
-	echo $file
+	echo "$file"
 done
 
 # here's a for loop with numbers
 for number in {1..10}
 do
-	echo Number is $number
+	echo Number is "$number"
 	echo The pause is done, moving to the next loop
 	echo ----------
 done
 
 for seqfile in *.fasta
 do
-	echo In $seqfile there are $(grep -c ">" $seqfile) sequences
-	echo In this directory there are $(ls *.fasta | wc -l) fasta files
-	echo In this directory there are $(ls *.txt | wc -l) text files
+	echo In "$seqfile" there are "$(grep -c ">" "$seqfile")" sequences
+	echo In this directory there are "$(find -- *.fasta | wc -l)" fasta files
+	echo In this directory there are "$(find -- *.txt | wc -l)" text files
 done
